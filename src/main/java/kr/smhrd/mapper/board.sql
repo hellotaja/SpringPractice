5a9809bbe6aca34e876dd66148e4e7af5e457c3f
SM2122 <SM2122@DESKTOP-IIR1TFJ>
2021-08-06 12:14:13 +0900

    sql

--- src/main/java/kr/smhrd/mapper/board.sql
+++ src/main/java/kr/smhrd/mapper/board.sql
@@ -2,8 +2,22 @@
 -- mysql, oracle : 표준-SQL
 
 create table tbl_board(
+   idx int not null auto_increment,
+   title varchar(100) not null,
+   contents varchar(2000) not null,
+   count int default 0,
+   writer varchar(100) not null,
+   indate datetime default now(),
+   primary key(idx)
+);
 
+insert into tbl_board(title, contents, writer)
+values('스프링 게시판', '스프링게시판', '관리자');
 
-)
+insert into tbl_board(title, contents, writer)
+values('스프링 게시판', '스프링게시판', '권누리');
+
+select * from tbl_board;
+
 
 
